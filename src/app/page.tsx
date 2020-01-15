d6f24be57e9b985634a82c3b12e2250967d775e3
rajann44 <rajann44@gmail.com>
2020-01-15 12:01:00 +0100

    Add slider component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    <div className="slider">
+      <h2>New slider Component</h2>
+    </div>
+    
     <div className="carousel">
       <h2>New carousel Component</h2>
     </div>
