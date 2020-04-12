40b4aecd9434261f15314e8eeebd1758e56cb7c1
rajann44 <rajann44@gmail.com>
2020-04-12 16:36:00 +0200

    Add map component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    <div className="map">
+      <h2>New map Component</h2>
+    </div>
+    
     <div className="chart">
       <h2>New chart Component</h2>
     </div>
