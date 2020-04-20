4dff757f4ccc0bcc9da20646a14c6131806a1672
rajann44 <rajann44@gmail.com>
2020-04-20 16:00:00 +0200

    Add menu component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    <div className="menu">
+      <h2>New menu Component</h2>
+    </div>
+    
     <div className="tree">
       <h2>New tree Component</h2>
     </div>
