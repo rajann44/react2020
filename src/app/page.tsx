09fee376491862b28fd116f42e565f7822b67b7e
rajann44 <rajann44@gmail.com>
2020-04-27 16:00:00 +0200

    Add tabs component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    <div className="tabs">
+      <h2>New tabs Component</h2>
+    </div>
+    
     <div className="menu">
       <h2>New menu Component</h2>
     </div>
