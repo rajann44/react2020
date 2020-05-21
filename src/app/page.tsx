160213c4e124fbacfce9b1977b54a6977c5e1a01
rajann44 <rajann44@gmail.com>
2020-05-21 16:09:00 +0200

    Add timepicker component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    <div className="timepicker">
+      <h2>New timepicker Component</h2>
+    </div>
+    
     <div className="datepicker">
       <h2>New datepicker Component</h2>
     </div>
