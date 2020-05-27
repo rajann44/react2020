eb418ed656c60a4b561dc07872ec350515499377
rajann44 <rajann44@gmail.com>
2020-05-27 09:32:00 +0200

    Add textarea component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    <div className="textarea">
+      <h2>New textarea Component</h2>
+    </div>
+    
     <div className="select">
       <h2>New select Component</h2>
     </div>
