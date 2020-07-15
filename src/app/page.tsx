bdf4879af3e3316a7eb624de34822f9c2a5700fa
rajann44 <rajann44@gmail.com>
2020-07-15 13:45:00 +0200

    Add modal component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    <div className="modal">
+      <h2>New modal Component</h2>
+    </div>
+    
     <div className="menu">
       <h2>New menu Component</h2>
     </div>
