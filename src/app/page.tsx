c2e53f07a51b4e4312e225226aaf878bb08313e9
rajann44 <rajann44@gmail.com>
2020-08-09 17:02:00 +0200

    Add alert component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    <div className="alert">
+      <h2>New alert Component</h2>
+    </div>
+    
     <div className="spinner">
       <h2>New spinner Component</h2>
     </div>
