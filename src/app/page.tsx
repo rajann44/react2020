e077f40f13291854474c2029157170d658279e2a
rajann44 <rajann44@gmail.com>
2020-08-25 17:28:00 +0200

    Add preview component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    <div className="preview">
+      <h2>New preview Component</h2>
+    </div>
+    
     <div className="viewer">
       <h2>New viewer Component</h2>
     </div>
