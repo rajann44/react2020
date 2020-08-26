a66559b2c3dc1c374f136d530803f9db8b2217ff
rajann44 <rajann44@gmail.com>
2020-08-26 18:08:00 +0200

    Add sort component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    <div className="sort">
+      <h2>New sort Component</h2>
+    </div>
+    
     <div className="filter">
       <h2>New filter Component</h2>
     </div>
