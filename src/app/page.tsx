20f1e7ad7807994c9dfbf07eaee660545779b8e0
rajann44 <rajann44@gmail.com>
2020-09-09 11:03:00 +0200

    Add menu component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    <div className="menu">
+      <h2>New menu Component</h2>
+    </div>
+    
     <div className="navigation">
       <h2>New navigation Component</h2>
     </div>
