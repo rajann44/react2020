c6a0122436c764c8f27241d90587fbb7cc749240
rajann44 <rajann44@gmail.com>
2020-10-04 18:37:00 +0200

    Add file-upload component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    <div className="file-upload">
+      <h2>New file-upload Component</h2>
+    </div>
+    
     <div className="textarea">
       <h2>New textarea Component</h2>
     </div>
