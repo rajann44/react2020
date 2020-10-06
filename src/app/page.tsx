19908e9c41370b2d87c72ed91c55811a6fbe2a28
rajann44 <rajann44@gmail.com>
2020-10-06 09:02:00 +0200

    Add progress component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    <div className="progress">
+      <h2>New progress Component</h2>
+    </div>
+    
     <div className="file-upload">
       <h2>New file-upload Component</h2>
     </div>
