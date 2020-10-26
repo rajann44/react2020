80d925895d153f6784bf6c3e1e0ca63671233daf
rajann44 <rajann44@gmail.com>
2020-10-26 16:00:00 +0100

    Add  component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -310,6 +310,10 @@ export default function Home() {
       <h2>New  Component</h2>
     </div>
     
+    <div className="">
+      <h2>New  Component</h2>
+    </div>
+    
     <div className="modal">
       <h2>New modal Component</h2>
     </div>
